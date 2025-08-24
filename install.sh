428e164625d9a535d6c9b00c0ab1f91556aa402b
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-08-24 15:33:16 +0200

    🔨 🔊 add line breaks to 'installation script' for better readability

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ clone() {
     repo_name="nix-config"
     repo_url="https://github.com/samiuens/$repo_name.git"
 
+    echo ""
     echo "> setting up git repo..."
     if [ "$(uname)" == "Darwin" ]; then
         nix shell nixpkgs#git --command git clone $repo_url ~/$repo_name
@@ -27,10 +28,11 @@ if [ "$(uname)" == "Darwin" ]; then
   echo "this script will prepare the system for nix-darwin installation."
   
   # wait for confirmation
-  read -n 1 -s -r -p "[?] press any key to continue or ctrl+c to abort..."
   echo ""
+  read -n 1 -s -r -p "[?] press any key to continue or ctrl+c to abort..."
   
   # command line tools
+  echo ""
   echo "installing 'command line tools'."
   if [[ -e /Library/Developer/CommandLineTools/usr/bin/git ]]; then
     echo "'command line tools' already installed."
@@ -42,11 +44,13 @@ if [ "$(uname)" == "Darwin" ]; then
   fi
 
   # rosetta
+  echo ""
   echo "installing 'rosetta'."
   softwareupdate --install-rosetta --agree-to-license
   echo "successfully installed 'rosetta'".
 
   # determinate nix
+  echo ""
   echo "installing nix..."
   curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm
   
@@ -54,10 +58,12 @@ if [ "$(uname)" == "Darwin" ]; then
   clone
 
   # complete
+  echo ""
   echo "a shell restart is needed."
   echo "after that, run the following command to activate the nix configuration:"
   echo "sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .#"
   echo "enter the hostname corresponding to the config you want to apply."
+  echo ""
 
 elif [ -f /etc/NIXOS ]; then
   echo "> nixos detected..."
@@ -65,14 +71,16 @@ elif [ -f /etc/NIXOS ]; then
   echo "this script will apply the nix configuration to your system."
 
   # wait for confirmation
-  read -n 1 -s -r -p "[?] press any key to continue or ctrl+c to abort..."
   echo ""
+  read -n 1 -s -r -p "[?] press any key to continue or ctrl+c to abort..."
 
   # clone git repository
   clone
 
+  echo ""
   hostname=$(input "> please provide the hostname, corresponding to the config you want to apply: ")
 
+  echo ""
   echo "> reading nix configuration..."
   if [ ! -d "./hosts/$hostname" ]; then
     echo "[!] no configuration found for $hostname in this repo."
@@ -81,12 +89,14 @@ elif [ -f /etc/NIXOS ]; then
   echo "found nix configuration."
  
   # copying hardware configuration
+  echo ""
   echo "> generating hardware config..."
-  sudo rm -r ./hosts/$hostname/hardware-configuration.nix
+  sudo rm -rf ./hosts/$hostname/hardware-configuration.nix
   sudo nixos-generate-config --dir ./hosts/$hostname
   echo "generated hardware configuration and copied into repo."
 
   # generate secure boot keys
+  echo ""
   confirm_sb=$(input "[?] are you planning to use 'secure boot' on this system (y/n): ")
   
   if [ $confirm_sb = "y" ]; then
@@ -98,7 +108,10 @@ elif [ -f /etc/NIXOS ]; then
     echo "> NOT using 'secure boot'"
   fi
   
+  echo ""
   sudo nixos-rebuild switch --flake ".#$hostname"
 else
+  echo ""
   echo "[!] this os is not supported by this configuration."
+  echo ""
 fi
