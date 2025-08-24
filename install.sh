97791bd00ac3e56777f7e767be0dbb33e93a5540
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-08-24 15:25:36 +0200

    🔨 add 'installation' script

---
+++ install.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+input() {
+  prompt=$1
+  input=""
+  read -p "$prompt" input
+  echo "$input"
+}
+
+clone() {
+    repo_name="nix-config"
+    repo_url="https://github.com/samiuens/$repo_name.git"
+
+    echo "> setting up git repo..."
+    if [ "$(uname)" == "Darwin" ]; then
+        nix shell nixpkgs#git --command git clone $repo_url ~/$repo_name
+    elif [ -f /etc/NIXOS ]; then
+        git clone $repo_url ~/$repo_name
+    fi
+    echo "cloned repo into home directory (under ~/$repo_name)"
+    cd ~/$repo_name
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+  echo "> macos detected..."
+  echo ""
+  echo "this script will prepare the system for nix-darwin installation."
+  
+  # wait for confirmation
+  read -n 1 -s -r -p "[?] press any key to continue or ctrl+c to abort..."
+  echo ""
+  
+  # command line tools
+  echo "installing 'command line tools'."
+  if [[ -e /Library/Developer/CommandLineTools/usr/bin/git ]]; then
+    echo "'command line tools' already installed."
+  else
+    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
+    softwareupdate -i "$PROD" --verbose
+    echo "successfully installed 'command line tools'".
+  fi
+
+  # rosetta
+  echo "installing 'rosetta'."
+  softwareupdate --install-rosetta --agree-to-license
+  echo "successfully installed 'rosetta'".
+
+  # determinate nix
+  echo "installing nix..."
+  curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm
+  
+  # clone git repository
+  clone
+
+  # complete
+  echo "a shell restart is needed."
+  echo "after that, run the following command to activate the nix configuration:"
+  echo "sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .#"
+  echo "enter the hostname corresponding to the config you want to apply."
+
+elif [ -f /etc/NIXOS ]; then
+  echo "> nixos detected..."
+  echo ""
+  echo "this script will apply the nix configuration to your system."
+
+  # wait for confirmation
+  read -n 1 -s -r -p "[?] press any key to continue or ctrl+c to abort..."
+  echo ""
+
+  # clone git repository
+  clone
+
+  hostname=$(input "> please provide the hostname, corresponding to the config you want to apply: ")
+
+  echo "> reading nix configuration..."
+  if [ ! -d "./hosts/$hostname" ]; then
+    echo "[!] no configuration found for $hostname in this repo."
+    exit 1
+  fi
+  echo "found nix configuration."
+ 
+  # copying hardware configuration
+  echo "> generating hardware config..."
+  sudo rm -r ./hosts/$hostname/hardware-configuration.nix
+  sudo nixos-generate-config --dir ./hosts/$hostname
+  echo "generated hardware configuration and copied into repo."
+
+  # generate secure boot keys
+  confirm_sb=$(input "[?] are you planning to use 'secure boot' on this system (y/n): ")
+  
+  if [ $confirm_sb = "y" ]; then
+    echo "> using 'secure boot'; generating keys..."
+    sudo nix-shell -p sbctl --command "sbctl create-keys" 
+    echo ""
+    echo "generated secure boot keys."
+  else
+    echo "> NOT using 'secure boot'"
+  fi
+  
+  sudo nixos-rebuild switch --flake ".#$hostname"
+else
+  echo "[!] this os is not supported by this configuration."
+fi
