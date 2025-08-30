5253d0c3dc655df40b71340d7fbf43c4a5e6e684
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-08-30 11:54:29 +0200

    🐛 fix 'cider' application using env variable

--- modules/home/applications/cider/samiuensay.nix
+++ modules/home/applications/cider/samiuensay.nix
@@ -1,22 +1,28 @@
 { pkgs, ... }:
 let
-  cider = pkgs.appimageTools.wrapType2 rec {
-    pname = "Cider";
-    version = "3.1.2";
-    src = ./cider-v3.1.2-linux-x64.AppImage; # Download the cider application and place it in this directory
+  ciderAppImagePath = builtins.getEnv "CIDER_APPIMAGE";
 
-    extraInstallCommands =
-      let
-        contents = pkgs.appimageTools.extract { inherit pname version src; };
-      in
-      ''
-        install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
-        substituteInPlace $out/share/applications/${pname}.desktop \
-          --replace 'Exec=AppRun' 'Exec=${pname}'
-        cp -r ${contents}/usr/share/icons $out/share
-      '';
-  };
+  cider =
+    if ciderAppImagePath != null && ciderAppImagePath != "" then
+      pkgs.appimageTools.wrapType2 rec {
+        pname = "Cider";
+        version = "3.1.2";
+        src = ciderAppImagePath;
+
+        extraInstallCommands =
+          let
+            contents = pkgs.appimageTools.extract { inherit pname version src; };
+          in
+          ''
+            install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
+            substituteInPlace $out/share/applications/${pname}.desktop \
+              --replace 'Exec=AppRun' 'Exec=${pname}'
+            cp -r ${contents}/usr/share/icons $out/share
+          '';
+      }
+    else
+      null;
 in
 {
-  home.packages = [ cider ];
+  home.packages = pkgs.lib.optionals (cider != null) [ cider ];
 }
