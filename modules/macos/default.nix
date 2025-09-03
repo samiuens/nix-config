86585562b1774d56cc68c9405addfca1367e2dc6
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-09-03 15:04:32 +0200

    ✨ add 'macos' imports

---
+++ modules/macos/default.nix
@@ -0,0 +1,30 @@
+{
+  inputs,
+  hostname,
+  hostConfig,
+  ...
+}:
+let
+  coreModules = map (name: ./core/${name}.nix) hostConfig.coreModules;
+  settingModules = map (name: ./settings/${name}.nix) hostConfig.settingModules;
+  applicationModules = map (name: ./applications/${name}/default.nix) hostConfig.systemApplications;
+  serviceModules = map (name: ./services/${name}/default.nix) hostConfig.systemServices;
+in
+{
+  imports =
+    [
+      # Host-specific configuration import
+      ../../hosts/${hostname}
+
+      # Flake imports
+      inputs.home-manager.darwinModules.home-manager
+      inputs.nix-homebrew.darwinModules.nix-homebrew
+
+      # Users
+      ./users/samiuensay.nix
+    ]
+    ++ coreModules
+    ++ settingModules
+    ++ applicationModules
+    ++ serviceModules;
+}
