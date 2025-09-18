1b1414b91d6d1cfc06a4e95ca28a8377cb9d542f
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-09-18 22:31:53 +0200

    🐛 fix hyprland system suspend

--- modules/home/gui/hyprland/plugins/hypridle.nix
+++ modules/home/gui/hyprland/plugins/hypridle.nix
@@ -15,17 +15,26 @@
       listener = [
         {
           timeout = 180; # 3 mins
-          #on-timeout = "loginctl lock-session";
-          on-timeout = "qs ipc call lock lock";
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
         }
         {
-          timeout = 300; # 5 mins
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          timeout = 180; # 3 mins
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
         }
         {
           timeout = 600; # 10 mins
-          on-timeout = "systemctl suspend-then-hibernate || loginctl suspend";
+          on-timeout = "qs ipc call lock lock";
+        }
+        {
+          timeout = 630; # 10.5 mins
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+        }
+        {
+          timeout = 900; # 15 mins
+          on-timeout = "systemctl suspend";
         }
       ];
     };
